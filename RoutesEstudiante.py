aeafab9ceec0359a770734643f0a0ea012fa0bd0
Oskreduard <oscar.avila.ing@gmail.com>
2022-10-21 12:41:58 -0500

    Creating Models and Controls

---
+++ RoutesEstudiante.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import jsonify
+from flask import request
+from flask_cors import CORS
+import json
+from __main__ import app
+
+from Controladores.ControladorEstudiante import ControladorEstudiante
+miControladorEstudiante=ControladorEstudiante()
+
+@app.route("/estudiantes",methods=['GET'])
+def getEstudiantes():
+    json=miControladorEstudiante.index()
+    return jsonify(json)
+@app.route("/estudiantes",methods=['POST'])
+def crearEstudiante():
+    data = request.get_json()
+    json=miControladorEstudiante.create(data)
+    return jsonify(json)
+@app.route("/estudiantes/<string:id>",methods=['GET'])
+def getEstudiante(id):
+    json=miControladorEstudiante.show(id)
+    return jsonify(json)
+@app.route("/estudiantes/<string:id>",methods=['PUT'])
+def modificarEstudiante(id):
+    data = request.get_json()
+    json=miControladorEstudiante.update(id,data)
+    return jsonify(json)
+@app.route("/estudiantes/<string:id>",methods=['DELETE'])
+def eliminarEstudiante(id):
+    json=miControladorEstudiante.delete(id)
+    return jsonify(json)
